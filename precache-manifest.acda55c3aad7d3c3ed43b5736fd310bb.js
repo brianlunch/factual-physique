de2f4799b9481787549b044a7c9f6371d47614c6
brianlunch <blynch5@tcd.ie>
2020-08-30 17:27:59 +0100

    Updates

---
+++ precache-manifest.acda55c3aad7d3c3ed43b5736fd310bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa73859421cadf59ee229e87b4c2481",
+    "url": "/factual-physique/index.html"
+  },
+  {
+    "revision": "ae02f63eca556391c738",
+    "url": "/factual-physique/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "20d6867b146bc7933001",
+    "url": "/factual-physique/static/css/main.bc79bc2e.chunk.css"
+  },
+  {
+    "revision": "ae02f63eca556391c738",
+    "url": "/factual-physique/static/js/2.ee143fb3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/factual-physique/static/js/2.ee143fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d6867b146bc7933001",
+    "url": "/factual-physique/static/js/main.4975da40.chunk.js"
+  },
+  {
+    "revision": "8243ff7328742aa8b66f",
+    "url": "/factual-physique/static/js/runtime-main.ee5ff598.js"
+  },
+  {
+    "revision": "7e947fe0b5db8bb4d7224588e96e4141",
+    "url": "/factual-physique/static/media/HomePageImageCut.7e947fe0.jpg"
+  }
+]);
